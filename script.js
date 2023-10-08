0b9ac1884daccb050c73608ea7bef5eb32d02f9a
Mwaniki <102294424+Aihhhhhh@users.noreply.github.com>
2023-10-08 07:33:51 -0700

    create functions for addition, subtraction, multiplication. division and test them on console

---
+++ script.js
@@ -0,0 +1,12 @@
+// function for adding numbers
+const add = (num1, num2) => num1 + num2;
+console.log(add(5.8,3));
+//function for subtracting numbers
+const subtract = (num1,num2) => num1 - num2;
+console.log(subtract(0,1));
+//function for multiplying two numbers
+const multiply = (num1,num2) =>  (num1 * num2);
+console.log(multiply(4,6));
+//function for dividing numbers
+const divide = (num1, num2) => num1/num2;
+console.log(divide(8,5));
