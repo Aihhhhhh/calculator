b2881cc3f305cc375e178287e306d0a2591edfca
Mwaniki <102294424+Aihhhhhh@users.noreply.github.com>
2023-10-12 07:13:26 -0700

    Declare variables to hold the numbers and operator

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 // function for adding numbers
 const add = (num1, num2) => num1 + num2;
-console.log(add(5.8,3));
+console.log(add(4.8,3));
 //function for subtracting numbers
 const subtract = (num1,num2) => num1 - num2;
-console.log(subtract(0,1));
+console.log(subtract(-6,1));
 //function for multiplying two numbers
 const multiply = (num1,num2) =>  (num1 * num2);
 console.log(multiply(4,6));
 //function for dividing numbers
 const divide = (num1, num2) => num1/num2;
 console.log(divide(8,5));
+
+//create variables for the operation process which includes a number, operator and another number
+//create first variable to hold first number
+let num1;
+//declare operator
+let operator;
+//declare second variable to hold second number
+let num2;
+
+//create an operate function that takes an operator and two numbers and then calls on of the above functions on the numbers
+const ope
+
+
+
