73515f354ac77cd1e14431d47bbd33e835afb347
Mwaniki <102294424+Aihhhhhh@users.noreply.github.com>
2023-10-13 07:22:18 -0700

    Query select all the buttons and attach event listeners

--- script.js
+++ script.js
@@ -19,8 +19,41 @@ let operator;
 //declare second variable to hold second number
 let num2;
 
-//create an operate function that takes an operator and two numbers and then calls on of the above functions on the numbers
-const operate = (num1,operator,num2) => add || subtract || multiply || divide
-
-
-
+//create function operate that takes two numbers and an operator
+//use DOM manipulation to get all the buttons
+const calckeys = document.querySelectorAll(".number");
+console.log(calckeys);
+//add event listener 'click' to the numbers
+for(let i=0;i<calckeys.length;i++){
+    console.log(calckeys[i])
+}
+calckeys.forEach(calc =>{
+    calc.addEventListener("click", ()=>{
+        console.log(calc)
+    })
+});
+//target the maths operations
+const operations = document.querySelectorAll(".calculator-key");
+console.log(operations);
+//attach an event listener to listen for clicks
+operations.forEach(oper =>{
+     oper.addEventListener("click", ()=>{
+        console.log(oper)
+    })
+});
+//target the equals sign
+const equalls = document.querySelector(".equals");
+console.log(equalls);
+//attach an event listener to listen to the event click
+equalls.addEventListener("click",()=>{
+    console.log(equalls)
+})
+//target the clear button
+const clears = document.querySelector(".clear");
+console.log(clears);
+//attach an event listener
+clears.addEventListener("click",()=>{
+    console.log(clears);
+})
+const view = document.querySelector(".dissplay");
+console.log(view);
