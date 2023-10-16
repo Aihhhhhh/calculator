1b4b42db97a9f3399d2bda86362e1565c408db59
Mwaniki <102294424+Aihhhhhh@users.noreply.github.com>
2023-10-16 09:19:20 -0700

    Edit the operate function to use switch statements and call either of the defined calculation functions like add, subtract, multiply, divide

--- script.js
+++ script.js
@@ -15,32 +15,30 @@ console.log(divide(8,5));
 //create first variable to hold first number
 let num1= null;
 //declare operator
-let operator=null;
+let operator= ['+','-','*','/'];
 //declare second variable to hold second number
 let num2=null;
 //store result
-let result = null;
-//create function operate that takes two numbers and an operator
-let currentDisplay = 0;
+//create function ate that takes two numbers and an operator
 //declare current operation
-let currentOperation = null;
-
-let displayValue = document.getElementById("userinput");
-
+let displayValue = document.getElementById("result");
+//store results
+/*const dis = (val) =>{
+    document.getElementById("result").value += val
+    displayValue = val
+    console.log(val)
+}
+*/
 //use DOM manipulation to get all the buttons
 const calckeys = document.querySelectorAll(".number");
 console.log(calckeys);
 calckeys.forEach(calc => {
     calc.addEventListener("click",()=>{
-        console.log(calc.innerHTML);
-        displayValue.placeholder+= calc.innerHTML
+        displayValue.value+= calc.innerHTML;
+        console.log(displayValue.value)
     })
 })
-
-
-
 //add event listener 'click' to the nu
-
 //target the maths operations
 const operations = document.querySelectorAll(".calculator-key");
 console.log(operations);
@@ -48,55 +46,56 @@ console.log(operations);
 operations.forEach(oper =>{
      oper.addEventListener("click",()=>{
         console.log(oper.innerHTML)
-        operator = oper.innerHTML
-        displayValue.placeholder += oper.innerHTML
+        displayValue.value += oper.innerHTML
     })
 });
 //create operate function that takes two numbers and an operator
-const operate = (num1,num2,operator) =>{
-    result=0;
-    let operationResult;
-    if(operator= '+'){
-        operationResult =add(num1,num2);
-        result+= operationResult
-        displayValue.placeholder = result
+const operate = (num1,num2,operator) => {
+    switch(operator){
+        case '+':
+            displayValue.value = add(num1,num2);
+            console.log(displayValue.value);
+            break;
+        case  '-':
+            displayValue.value = subtract(num1,num2);
+            console.log(displayValue.value);
+            break;
+        case '*':
+            displayValue.value = multiply(num1,num2);
+            console.log(displayValue.value);
+            break;
+        case '/':
+            displayValue.value = divide(num1,num2);
+            console.log(displayValue.value);
+            break;
+        default:
+            displayValue.value = null;
+            console.log(displayValue.value)
     }
-    else if(operator=== '-'){
-        operationResult = subtract(num1,num2)
-        result+= operationResult
-    }
-    else if(operator === '*'){
-        operationResult= multiply(num1,num2)
-        result +=operationResult
-    }
-    else {
-        operationResult= divide(num1,num2);
-        result +=operationResult;
-    };
-    return result;
 }
-console.log(operate(2,3,'/'))
+operate();
 //target the equals sign
 const equalls = document.querySelector(".equals");
 console.log(equalls);
 //attach an event listener to listen to the event click
 equalls.addEventListener("click",()=>{
-    displayValue.placeholder = operate()
     console.log(equalls.innerHTML)
     
+    console.log(displayValue.value)
 })
 //target the clear button
 const clears = document.querySelector(".clear");
 console.log(clears);
 //attach an event listener
 clears.addEventListener("click",()=>{
-    displayValue.placeholder=""
+    displayValue.value = null;
     console.log(clears.innerHTML);
 })
 //target the decimal point
 const decimals = document.querySelector(".point");
 console.log(decimals);
+//add an event listener to capture the float and update the value on the display
 decimals.addEventListener("click",()=>{
-    displayValue.placeholder+= decimals.innerHTML;
-    console.log(decimals.innerHTML)
+    displayValue.value+= decimals.innerHTML;
+    console.log(decimals.innerHTML);
 })
